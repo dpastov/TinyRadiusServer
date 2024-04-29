4f4299d31d085db68970939ee883cb9710dbbd87
dpastov <dpastov@gmail.com>
2024-04-29 16:27:25 +0200

    released first version of server

--- src/main/java/MyTinyRadiusServer.java
+++ src/main/java/MyTinyRadiusServer.java
@@ -9,30 +9,35 @@ public class MyTinyRadiusServer extends RadiusServer {
 
 	@Override
 	public String getSharedSecret(InetSocketAddress client) {
-    	System.out.println("-- getSharedSecret2 --");
-        return "sharedSecret123";
+		return "sharedSecret123";
 	}
 
 	@Override
 	public String getUserPassword(String userName) {
-    	System.out.println("-- getUserPassword --");
 		if (userName.equals("John")) {
-			return "p123";
+			return "token123";
 		}
-		
+
 		return null;
 	}
 
-    public RadiusPacket accessRequestReceived(AccessRequest request, InetAddress client) {
-    	System.out.println("-- accessRequestReceived --");
-        String username = request.getUserName();
-        String password = request.getUserPassword();
-        
-        if (username.equals("John") && password.equals("token123")) {
-        	RadiusPacket response = new RadiusPacket(RadiusPacket.ACCESS_ACCEPT);
-        	return response;
-        }
-        
-        return null;
-    }
+	private RadiusPacket accessRequestReceived(AccessRequest request) {
+		String username = request.getUserName();
+		String password = request.getUserPassword();
+
+		if (password.equals(getUserPassword(username))) {
+			RadiusPacket accessAcceptPacket = new RadiusPacket(RadiusPacket.ACCESS_ACCEPT, request.getPacketIdentifier());
+			return accessAcceptPacket;
+		}
+
+		return new RadiusPacket(RadiusPacket.ACCESS_REJECT, request.getPacketIdentifier());	
+	}
+
+	public RadiusPacket accessRequestReceived(AccessRequest request, InetSocketAddress client) {
+		return accessRequestReceived(request);
+	}
+
+	public RadiusPacket accessRequestReceived(AccessRequest request, InetAddress client) {
+		return accessRequestReceived(request);
+	}
 }
